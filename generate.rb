70aa27c437a9eefbff3858fe18b69a3db6f6691f
NAGAMINE Hideaki <1995.hnagamin@gmail.com>
2021-02-11 22:13:46 +0900

    s/; /;/

--- generate.rb
+++ generate.rb
@@ -9,6 +9,6 @@ outfile = File.open('dict.txt', 'w', encoding: 'EUC-JP')
 data.each do |row|
     word = row['word']
     key = word.downcase.gsub(/[^a-z]/, '')
-    annot = row['annot'] ? "; #{row['annot']}" : ""
+    annot = row['annot'] ? ";#{row['annot']}" : ""
     outfile.puts "#{key} /#{word}#{annot}/"
 end

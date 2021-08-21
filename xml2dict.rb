1cf715f2b9f2e28af5926d8252ea9805136460b8
NAGAMINE Hideaki <1995.hnagamin@gmail.com>
2021-08-21 12:56:15 +0900

    Introduce dict/tex.xml

--- xml2dict.rb
+++ xml2dict.rb
@@ -4,13 +4,17 @@ require 'nokogiri'
 
 outfile = File.open('dict.txt', 'w', encoding: 'EUC-JP')
 
-doc = Nokogiri::XML(File.open('dict.xml', 'r', encoding: 'EUC-JP'))
+sources = ['dict.xml', 'dict/tex.xml']
 
-doc.xpath('/jisyo/ca').each do |ca|
-  word = ca.at('word').text
-  yomi = ca.at('yomi')&.text || word.downcase.gsub(/[^a-z]/, '')
+sources.each do |filename|
+  doc = Nokogiri::XML(File.open(filename, 'r', encoding: 'EUC-JP'))
 
-  annotation = ca.at('annotation')&.text
-  annot = annotation ? ";#{annotation}" : ''
-  outfile.puts "#{yomi} /#{word}#{annot}/"
+  doc.xpath('/jisyo/ca').each do |ca|
+    word = ca.at('word').text
+    yomi = ca.at('yomi')&.text || word.downcase.gsub(/[^a-z]/, '')
+
+    annotation = ca.at('annotation')&.text
+    annot = annotation ? ";#{annotation}" : ''
+    outfile.puts "#{yomi} /#{word}#{annot}/"
+  end
 end
